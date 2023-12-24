95be64e2fa0d6256b0f0fc2bd98767136dab4459
Hira-Yuki <cjsdlf44@gmail.com>
2023-12-24 16:45:55 +0900

    상품 업로드를 위한 UI 생성

---
+++ src/app/products/upload/page.tsx
@@ -0,0 +1,82 @@
+'use client'
+
+import Button from '@/app/components/Button'
+import Container from '@/app/components/Container'
+import Heading from '@/app/components/Heading'
+import Input from '@/app/components/Input'
+import React, { useState } from 'react'
+import { FieldValues, SubmitHandler, useForm } from 'react-hook-form'
+
+const ProductUploadPage = () => {
+  const [isLoading, setIsLoading] = useState(false)
+
+  const { register, handleSubmit, setValue, watch, formState: { errors, }, reset } = useForm<FieldValues>({
+    defaultValues: {
+      title: "",
+      discription: "",
+      category: "",
+      latitude: 33.5563,
+      logitude: 126.79581,
+      imageSrc: "",
+      price: 1
+    }
+
+  })
+
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+
+  }
+
+  return (
+    <Container>
+      <div className='max-w-screen-lg mx-auto'>
+        <form
+          className=' flex flex-col gap-8'
+          onSubmit={handleSubmit(onSubmit)}
+        >
+          <Heading
+            title="Product Upload"
+            subtitle="upload your product"
+          />
+          <Input
+            id="title"
+            label='Title'
+            disabled={isLoading}
+            register={register}
+            errors={errors}
+            required
+          />
+          <hr />
+          <Input
+            id="discription"
+            label='Discription'
+            disabled={isLoading}
+            register={register}
+            errors={errors}
+            required
+          />
+          <hr />
+          <Input
+            id="price"
+            label='Price'
+            formatPrice
+            disabled={isLoading}
+            register={register}
+            errors={errors}
+            required
+          />
+          <hr />
+          <div className='grid grid-cols-1 md:grid-cols-2 gap-3 max-h-[50vh] overflow-y-auto'>
+            {/* 카테고리 영역 */}
+          </div>
+          <hr />
+          {/* 지도 영역 */}
+
+          <Button label='상품 생성하기' />
+        </form>
+      </div>
+    </Container>
+  )
+}
+
+export default ProductUploadPage
