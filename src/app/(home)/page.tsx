96d6fff04bf0c8871ebe43a52741395d0f575fda
Hira-Yuki <cjsdlf44@gmail.com>
2024-01-05 22:00:28 +0900

    홈 화면 UI 작성하기

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,5 +1,10 @@
 import Image from 'next/image'
 import getProducts, { ProductsParams } from '../actions/getProducts'
+import Container from '../components/Container'
+import EmptyState from '../components/EmptyState'
+import ProductCard from '../components/ProductCard'
+import getCurrentUser from '../actions/getCurrentUser'
+import FloatingButton from '../components/FloatingButton'
 
 interface HomeProps {
   searchParams: ProductsParams
@@ -8,9 +13,31 @@ interface HomeProps {
 export default async function Home({ searchParams }: HomeProps) {
 
   const products = await getProducts(searchParams)
+  const currentUser = await getCurrentUser()
+
+  console.log("products?????? ", products)
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      루트 페이지
-    </main>
+    <Container>
+      { /* Category */}
+
+      {
+        products?.data.length === 0
+          ?
+          <EmptyState />
+          :
+          <>
+            <div className='grid grid-cols-1 gap-8 pt-12 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 2lx:grid-cols-6'>
+              {products?.data.map((product) => {
+                return <ProductCard
+                  currentUser={currentUser}
+                  key={product.id}
+                  data={product}
+                />
+              })}
+            </div>
+          </>
+      }
+      <FloatingButton href="/products/upload">+</FloatingButton>
+    </Container>
   )
 }
